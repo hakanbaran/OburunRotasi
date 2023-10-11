249054197e26f76a8c77c890911696c14f7dc317
Hakan Baran <hakanbaran.developer@gmail.com>
2023-10-11 12:41:47 +0300

    First Commit...

---
+++ OburunRotasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OburunRotasi
+//
+//  Created by Hakan Baran on 11.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
