c22c134c1dddc5fcc6bfe42cc40d86b4353db995
Hakan Baran <hakanbaran.developer@gmail.com>
2023-10-11 14:31:36 +0300

    initial settings...

---
+++ OburunRotasi/Views/ViewControllers/TabBarVC.swift
@@ -0,0 +1,32 @@
+//
+//  TabBarVC.swift
+//  OburunRotasi
+//
+//  Created by Hakan Baran on 11.10.2023.
+//
+
+import UIKit
+
+class TabBarVC: UITabBarController {
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let homeVC = HomeVC()
+        
+        homeVC.navigationItem.largeTitleDisplayMode = .always
+        
+        let navHome = UINavigationController(rootViewController: homeVC)
+        
+        navHome.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "person"), tag: 1)
+        
+        setViewControllers([navHome], animated: true)
+        
+        
+    }
+    
+
+}
