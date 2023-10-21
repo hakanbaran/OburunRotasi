8b022fbf75e15abaf6c2644933b2bd8526d85931
Hakan Baran <hakanbaran.developer@gmail.com>
2023-10-22 01:26:48 +0300

    Add Appicon...

--- OburunRotasi/ViewControllers/Core/TabBarVC.swift
+++ OburunRotasi/ViewControllers/Core/TabBarVC.swift
@@ -6,15 +6,32 @@
 //
 
 import UIKit
-
+import Foundation
 
 
 class TabBarVC: UITabBarController, UITabBarControllerDelegate {
+    
+    let btnMiddle : UIButton = {
+           let btn = UIButton(frame: CGRect(x: 0, y: 0, width: 60, height: 60))
+            btn.setTitle("", for: .normal)
+            btn.backgroundColor = UIColor(hex: "#fe989b")
+            btn.layer.cornerRadius = 30
+            btn.layer.shadowColor = UIColor.black.cgColor
+            btn.layer.shadowOpacity = 0.2
+            btn.layer.shadowOffset = CGSize(width: 4, height: 4)
+            btn.setBackgroundImage(UIImage(named: "ic_camera"), for: .normal)
+            return btn
+        }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         overrideUserInterfaceStyle = .dark
+        
+        btnMiddle.frame = CGRect(x: Int(self.tabBar.bounds.width)/2 - 30, y: -20, width: 60, height: 60)
+        
+//        self.tabBar.addSubview(btnMiddle)
+//        setupCustomTabBar()
         
         let homeVC = HomeVC()
         let favoriteVC = FavoriteVC()
@@ -33,12 +50,55 @@ class TabBarVC: UITabBarController, UITabBarControllerDelegate {
         let navBasket = UINavigationController(rootViewController: basketVC)
         let navProfile = UINavigationController(rootViewController: profileVC)
         
-        navHome.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "person"), tag: 1)
-        navFavorite.tabBarItem = UITabBarItem(title: "Favorite", image: UIImage(systemName: "person"), tag: 2)
-        navBasket.tabBarItem = UITabBarItem(title: "Basket", image: UIImage(systemName: "person"), tag: 3)
-        navProfile.tabBarItem = UITabBarItem(title: "Profile", image: UIImage(systemName: "person"), tag: 4)
+        navHome.tabBarItem = UITabBarItem(title: "Ana Sayfa", image: UIImage(systemName: "house"), tag: 1)
+        navFavorite.tabBarItem = UITabBarItem(title: "Favoriler", image: UIImage(systemName: "heart"), tag: 2)
+        navBasket.tabBarItem = UITabBarItem(title: "Sepetim", image: UIImage(systemName: "basket"), tag: 3)
+        navProfile.tabBarItem = UITabBarItem(title: "Ayarlar", image: UIImage(systemName: "gear"), tag: 4)
         setViewControllers([navHome,navFavorite,navBasket, navProfile], animated: true)
     }
+    
+    
+    func setupCustomTabBar() {
+            let path : UIBezierPath = getPathForTabBar()
+            let shape = CAShapeLayer()
+            shape.path = path.cgPath
+            shape.lineWidth = 3
+            shape.strokeColor = UIColor.white.cgColor
+            shape.fillColor = UIColor.white.cgColor
+            self.tabBar.layer.insertSublayer(shape, at: 0)
+            self.tabBar.itemWidth = 40
+            self.tabBar.itemPositioning = .centered
+            self.tabBar.itemSpacing = 180
+            self.tabBar.tintColor = UIColor(hex: "#fe989b")
+        }
+    
+    
+    
+    
+    
+    
+    func getPathForTabBar() -> UIBezierPath {
+            let frameWidth = self.tabBar.bounds.width
+            let frameHeight = self.tabBar.bounds.height + 20
+            let holeWidth = 150
+            let holeHeight = 50
+            let leftXUntilHole = Int(frameWidth/2) - Int(holeWidth/2)
+            
+            let path : UIBezierPath = UIBezierPath()
+            path.move(to: CGPoint(x: 0, y: 0))
+            path.addLine(to: CGPoint(x: leftXUntilHole , y: 0)) // 1.Line
+            path.addCurve(to: CGPoint(x: leftXUntilHole + (holeWidth/3), y: holeHeight/2), controlPoint1: CGPoint(x: leftXUntilHole + ((holeWidth/3)/8)*6,y: 0), controlPoint2: CGPoint(x: leftXUntilHole + ((holeWidth/3)/8)*8, y: holeHeight/2)) // part I
+            
+            path.addCurve(to: CGPoint(x: leftXUntilHole + (2*holeWidth)/3, y: holeHeight/2), controlPoint1: CGPoint(x: leftXUntilHole + (holeWidth/3) + (holeWidth/3)/3*2/5, y: (holeHeight/2)*6/4), controlPoint2: CGPoint(x: leftXUntilHole + (holeWidth/3) + (holeWidth/3)/3*2 + (holeWidth/3)/3*3/5, y: (holeHeight/2)*6/4)) // part II
+            
+            path.addCurve(to: CGPoint(x: leftXUntilHole + holeWidth, y: 0), controlPoint1: CGPoint(x: leftXUntilHole + (2*holeWidth)/3,y: holeHeight/2), controlPoint2: CGPoint(x: leftXUntilHole + (2*holeWidth)/3 + (holeWidth/3)*2/8, y: 0)) // part III
+            path.addLine(to: CGPoint(x: frameWidth, y: 0)) // 2. Line
+            path.addLine(to: CGPoint(x: frameWidth, y: frameHeight)) // 3. Line
+            path.addLine(to: CGPoint(x: 0, y: frameHeight)) // 4. Line
+            path.addLine(to: CGPoint(x: 0, y: 0)) // 5. Line
+            path.close()
+            return path
+        }
 }
 
 
